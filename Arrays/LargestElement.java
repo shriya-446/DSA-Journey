a9e93a738cd2539f019778c9c04b63b11f2245fb
shriya-446 <shreyar130613@gmail.com>
2026-09-02 21:48:36 +0530

    Added Second Largest solution

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,12 +1,22 @@
-public class LargestElement {
+public class SecondLargest {
     public static void main(String[] args) {
-        int[] arr = {3, 7, 2, 9, 5};
+
+        int[] arr = {10, 5, 8, 20, 3};
+
         int max = arr[0];
+        int sl = arr[0];
+
         for (int i = 1; i < arr.length; i++) {
+
             if (arr[i] > max) {
+                sl = max;
                 max = arr[i];
             }
+            else if (arr[i] > sl && arr[i] != max) {
+                sl = arr[i];
+            }
         }
-        System.out.println("Largest element: " + max);
+
+        System.out.println("Second largest: " + sl);
     }
 }
---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,9 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 5, 8, 20, 3};
+
+        
+
+    }
+}
