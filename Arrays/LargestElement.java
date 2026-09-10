c0a98c7f6b4a7b5ad0d0a58f8d7fe81bb572b451
shriya-446 <shreyar130613@gmail.com>
2026-09-10 22:42:30 +0530

    Updated array solution

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,7 +1,7 @@
 public class LargestElement {
     public static void main(String[] args) {
         int[] arr = {3, 7, 2, 9, 5};
-        int max = arr[0];
+        int max = arr[0]; // Assume the first element is the largest
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
